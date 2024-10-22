33f974cb99a0b1ba12ee36eb6d97f67262c4f3c3
Fernanda Ishida <fer.ishida@gmail.com>
2024-10-22 00:13:58 -0300

    Submit event and validating password

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email">
 
     <label>Password:</label>
     <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role: </label>
     <select v-model="role">
@@ -22,6 +23,10 @@
     <div class="terms">
         <input type="checkbox" v-model="terms" required>
         <label>Accept terms and coditions</label>
+    </div>
+
+    <div class="submit">
+        <button>Create an Account</button>
     </div>
 
 
@@ -44,7 +49,8 @@ export default {
             role: 'designer',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: ''
 
         }
     },
@@ -61,6 +67,19 @@ export default {
             this.skills = this.skills. filter((item) => {
                 return skill !== item
             })
+        },
+        handleSubmit() {
+            //validate password
+            this.passwordError = this.password.length > 5 ? '' : 'Password must be at least 6 chars long'
+
+            //just for elucidate form working
+            if (!this.passwordError) {
+                console.log('email: ', this.email)
+                console.log('password: ', this.password)
+                console.log('role: ', this.role)
+                console.log('skills: ', this.skills)
+                console.log('terms accepted: ', this.terms)
+            }
         }
     }
 }
@@ -112,5 +131,21 @@ export default {
         color: #777;
         cursor: pointer;
     }
-
+    button {
+        background: #0b6dff;
+        border: 0;
+        padding: .625rem 1.25rem;
+        margin-top: 1.25rem;
+        color: white;
+        border-radius: 1.25rem;
+    }
+    .submit {
+        text-align: center;  
+    }
+    .error {
+       color: #ff0062;
+       margin-top: .625rem;
+       font-size: 0,8em;
+       font-weight: bold; 
+    }
 </style>
